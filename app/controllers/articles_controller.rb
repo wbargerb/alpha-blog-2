d26815ef0c476ef65b63c51ed09a6cc080dba693
wbargerb <bargerbos@hotmail.com>
2020-02-21 23:42:36 +0100

    edit function added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,11 @@ class ArticlesController < ApplicationController
   def new
       @article = Article.new        
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -11,6 +16,19 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "Article was succesfully updated"
+        redirect_to article_path(@article)
+    else
+        render :edit
+    end
+  end
+
+
+
   def show
     @article = Article.find(params[:id])
   end
